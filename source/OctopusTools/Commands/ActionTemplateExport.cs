7a89e07789cd576a8325882a48ffa85abec0b208
Kenny Pitt <kenny.pitt@hp.com>
2014-11-03 16:48:42 -0500

    Add exporter for step templates

---
+++ source/OctopusTools/Commands/ActionTemplateExport.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Octopus.Client.Model;
+
+namespace OctopusTools.Commands
+{
+    public class ActionTemplateExport
+    {
+        public ActionTemplateResource ActionTemplate { get; set; }
+    }
+}
